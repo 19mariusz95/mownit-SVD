97ea4849bf42c9876319fbe2673ddbaa26a1d369
19mariusz95 <19mariusz95@gmail.com>
2016-05-02 00:07:05 +0200

    idf

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from scipy.sparse import dok_matrix
+from math import log
+
+
+def inverse_document_frequency(A, docn, n):
+    nw = [0] * n
+    for value in A.keys():
+        if A[value] > 0:
+            nw[value[0]] += 1
+    values = []
+    for value in A.keys():
+        idf = log(docn / nw[value[0]], 10)
+        if (idf != 0.0):
+            A[value[0], value[1]] *= idf
+        else:
+            values.append(value)
+    for val in values:
+        A[val] = 0.0
+    print("inverse document frequency done")
 
 file = open("data.txt", "r")
 
@@ -21,14 +39,16 @@ words = []
 
 A = dok_matrix((n, docn), dtype=float)
 
-for i in range(0,n):
+for i in range(0, n):
     line = file.readline()
     tmp = line.split(" ")
     words.append(tmp[0])
-    print(i, " ", n)
     for j in range(1, len(tmp) - 1):
         val = tmp[j]
         tmp2 = val.split(":")
         dok = int(tmp2[0])
         cnt = int(tmp2[1])
         A[i, dok] = cnt
+
+print("data loaded")
+inverse_document_frequency(A, docn, n)
